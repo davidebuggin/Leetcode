f3129bda2b4bc67e3ab8a6aadb27ed0c3a5ad4f8
DavideBuggin <davide.buggin@gmail.com>
2025-04-08 16:58:23 +0200

    Exercise 11 - Container with most water

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+public class ContainerWithMostWater {
+    
+    public static void main(String[] args) {
+        
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height)); 
+
+    }
+
+    public static int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+
+        int maxArea = 0;
+        
+        while(left < right){
+            
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+            
+            if(height[left] < height[right]){
+                left ++;
+            } else {
+                right --;
+            } 
+        }
+        
+        return maxArea;
+    }
+}
