163edccd361f336ee071b578965d6f2843c0d912
DavideBuggin <davide.buggin@gmail.com>
2025-04-08 18:39:21 +0200

    Exercise 12 -Integer To Roman

---
+++ IntegerToRoman.java
@@ -0,0 +1,22 @@
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        
+        int num = 1;
+        System.out.println(intToRoman(num));
+    }
+
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder romanNumber = new StringBuilder();
+
+        for(int i = 0; i < values.length; i++){
+            while(num >= values[i]){
+                num -= values[i];
+                romanNumber.append(symbols[i]);
+            }
+        }
+
+        return romanNumber.toString();
+    }
+}
