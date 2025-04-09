fff65dcd16c1b9461cfc94cb384fc28d3844f828
DavideBuggin <davide.buggin@gmail.com>
2025-04-09 10:31:39 +0200

    Exercise 14 - Longest Common Prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+       
+        String [] strs = {"flowers", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs){
+        
+        if(strs == null || strs.length == 0) return "";
+        
+        StringBuilder longestPrefix = new StringBuilder();
+
+        for(int i = 0; i < strs[0].length(); i++){
+            char currentChar = strs[0].charAt(i);
+
+            for(int j = 1; j < strs.length; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
+                    return longestPrefix.toString();
+                }
+            }
+
+            longestPrefix.append(currentChar);
+        }
+
+        return longestPrefix.toString();
+    }
+}
