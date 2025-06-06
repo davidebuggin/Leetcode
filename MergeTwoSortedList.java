bfc3fa45e6b0b5a9483c8d253d61a936bb164221
DavideBuggin <davide.buggin@gmail.com>
2025-06-06 16:17:58 +0200

    Exercise 21 - Merge Two Sorted Lists + ListNode Class

---
+++ MergeTwoSortedList.java
@@ -0,0 +1,53 @@
+public class MergeTwoSortedList {
+    
+
+    public static void main(String[] args) {
+        
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+        ListNode merged = mergeTwoLists(list1, list2);
+        printList(merged);
+
+    }
+
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                current.next = p1;
+                p1 = p1.next; 
+            } else {
+                current.next = p2;
+                p2 = p2.next;
+            }
+            current= current.next;
+        }  
+
+        current.next = (p1 != null) ? p1 : p2;
+        
+        
+        return dummy.next; 
+    }
+    
+    public static void printList(ListNode head) {
+    while (head != null) {
+        System.out.print(head.val);
+        if (head.next != null) System.out.print(" -> ");
+        head = head.next;
+    }
+    System.out.println();
+}
+
+}
+
