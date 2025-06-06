d066ab15827b0ae37c974a953576888e201e4111
DavideBuggin <davide.buggin@gmail.com>
2025-06-06 15:16:41 +0200

    Exercise 20 - Valid Parentheses

---
+++ ValidParentheses.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class ValidParentheses {
+    
+
+    public static void main(String [] args) {
+        String s = "]";
+        System.out.println(isValid(s));
+
+    }
+
+    public static boolean isValid (String s) {
+        Set<Character> openPar = new HashSet<>();
+        Deque<Character> stack = new ArrayDeque<Character>();
+        Map<Character, Character> matching = new HashMap<>();
+        matching.put(')', '(');
+        matching.put(']', '[');
+        matching.put('}', '{');
+        
+        openPar.add('(');
+        openPar.add('[');
+        openPar.add('{');
+        for(char c : s.toCharArray()) {
+            if(openPar.contains(c)){
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()){ 
+                    return false;
+                }
+                
+                if(stack.peek() == matching.get(c)){
+                    stack.pop();
+                } else{
+                    return false;
+                }
+            }
+        }
+        
+        if (stack.isEmpty()){
+            return true;
+        } 
+        return false;
+    }
+}
