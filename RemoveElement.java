97305a520aa98ddbb291e50398485b6538ffeea3
DavideBuggin <davide.buggin@gmail.com>
2025-06-12 15:47:29 +0200

    Exercise 27 - Remove Element

---
+++ RemoveElement.java
@@ -0,0 +1,21 @@
+public class RemoveElement {
+    
+
+    public static void main(String[] args) {
+        int val = 3;
+        int [] nums = {3,2,2,3};
+        System.out.println(removeElement(nums, val));
+
+    }
+
+    public static int removeElement(int [] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+           if(nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+           }
+        }
+        return k;
+    }
+}
