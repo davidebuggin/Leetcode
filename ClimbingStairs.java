0ef1129be0a2c9606476abd24946f63ccbd0b171
DavideBuggin <davide.buggin@gmail.com>
2025-06-23 19:17:27 +0200

    Exercise 70 - Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,29 @@
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+
+        int n = 9;
+        System.out.println(climbStairs(n));
+    }
+
+    public static int climbStairs(int n) {
+
+        switch (n) {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            default:
+                int prev1 = 2;
+                int prev2 = 1;
+                for (int i = 3; i <= n; i++) {
+                    int current = prev1 + prev2;
+                    prev2 = prev1;
+                    prev1 = current;
+                }
+                return prev1;
+        }
+    }
+}
