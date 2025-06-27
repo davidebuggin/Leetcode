92124e8dac076e25f513475b8bf7a32edfeb43e3
DavideBuggin <davide.buggin@gmail.com>
2025-06-27 18:57:31 +0200

    Exercise 101 - Symmetric Tree

---
+++ SymmetricTree.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        System.out.println(isSymmetric(root));
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if (left == null && right == null)
+                continue;
+            if (left == null || right == null)
+                return false;
+            if (left.val != right.val)
+                return false;
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+
+        }
+        return true;
+    }
+}
