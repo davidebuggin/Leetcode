12aa21c4fb75bcbbc615c38d0bda9282b15068f4
DavideBuggin <davide.buggin@gmail.com>
2025-06-28 10:29:25 +0200

    #fix code

--- MaximumDepthofBinaryTree.java
+++ MaximumDepthofBinaryTree.java
@@ -1,22 +1,22 @@
 public class MaximumDepthofBinaryTree {
-    
+
     public static void main(String[] args) {
-        
+
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
         root.right = new TreeNode(20);
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
         System.out.println(maxDepth(root));
-        
+
     }
 
     public static int maxDepth(TreeNode root) {
 
-        if(root == null) {
+        if (root == null) {
             return 0;
         }
-        
+
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
