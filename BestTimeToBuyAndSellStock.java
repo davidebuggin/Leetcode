a31c47c5c8f05a392cdf19e5bc9fcb629945787b
DavideBuggin <davide.buggin@gmail.com>
2025-07-15 11:36:11 +0200

    Ex 121 - remove unused code and added problem statements

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,3 +1,7 @@
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
+
 public class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         int[] prices = { 1, 7, 6, 4, 3, 1 };
@@ -20,20 +24,3 @@ public class BestTimeToBuyAndSellStock {
 
 }
 
-// public static int maxProfit(int[] prices){
-// int maxProfit = 0;
-// for(int i = 0; i < prices.length; i++){
-// int buyday = prices[i];
-// for(int j = prices.length -1; j > i; j--){
-// int sellday = prices[j];
-// if(sellday > buyday){
-// if(sellday - buyday > maxProfit){
-// maxProfit = sellday - buyday;
-// }
-// }
-// }
-// }
-
-// return maxProfit;
-
-// }
