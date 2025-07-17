1adecebccdd4cb57093b6a14ae3c72b57c1a8603
DavideBuggin <davide.buggin@gmail.com>
2025-07-17 17:01:41 +0200

    Exercise 136 - Single Number

---
+++ SingleNumber.java
@@ -0,0 +1,17 @@
+/* Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space. */
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums){
+        int result = 0 ; 
+        for(int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
