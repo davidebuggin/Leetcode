88d2121b8f38e980baf6d55e62da2c103e850595
DavideBuggin <davide.buggin@gmail.com>
2025-07-18 17:15:47 +0200

    Exercise 141 - Linked Cycle

---
+++ LinkedListCycle.java
@@ -0,0 +1,36 @@
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.*/
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(0);
+        ListNode fourth = new ListNode(-4);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = second;
+
+        System.out.println(hasCycle(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
