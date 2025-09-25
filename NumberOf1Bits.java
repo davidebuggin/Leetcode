b539c8765de856ecf902145f37f330ec1660ea26
DavideBuggin <davide.buggin@gmail.com>
2025-09-25 12:30:44 +0200

    Exercise 191 - Number of 1 Bits

---
+++ NumberOf1Bits.java
@@ -0,0 +1,21 @@
+// Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+public class NumberOf1Bits {
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(hammingWeight(n));
+    }
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
