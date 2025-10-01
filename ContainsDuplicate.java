0c0bca68424b2994a25d1bf4540184c8444092e2
DavideBuggin <davide.buggin@gmail.com>
2025-10-01 15:54:28 +0200

    Exercise 217 - Contains Duplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,21 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> presentNum = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (presentNum.contains(nums[i])) {
+                return true;
+            }
+            presentNum.add(nums[i]);
+        }
+        return false;
+    }
+}
