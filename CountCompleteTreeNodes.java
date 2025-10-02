3a206885fa6f00e53ae89f3b7ba55344b35e181f
DavideBuggin <davide.buggin@gmail.com>
2025-10-02 18:02:44 +0200

    Exercise 222 - Count Complete Tree Node

---
+++ CountCompleteTreeNodes.java
@@ -0,0 +1,38 @@
+/* Given the root of a complete binary tree, return the number of the nodes in the tree.
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. 
+It can have between 1 and 2h nodes inclusive at the last level h.*/
+
+public class CountCompleteTreeNodes {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+
+        System.out.println(countNodes(root));
+    }
+
+    public static int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int hl = getHeight(root.left);
+        int hr = getHeight(root.right);
+        if (hl == hr) {
+            return (1 << hl) + countNodes(root.right);
+        } else {
+            return (1 << hr) + countNodes(root.left);
+        }
+    }
+
+    private static int getHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+}
