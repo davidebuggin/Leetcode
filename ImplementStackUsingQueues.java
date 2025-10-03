e9663599d91942eb8929329c9d20fe3c35fbd84f
DavideBuggin <davide.buggin@gmail.com>
2025-10-03 18:31:59 +0200

    Exercise 225 - Implement Stack Using Queues

---
+++ ImplementStackUsingQueues.java
@@ -0,0 +1,37 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueues {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public ImplementStackUsingQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q2.add(x);
+        while (!q1.isEmpty()) {
+            int topQ1 = q1.poll();
+            q2.add(topQ1);
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        return q1.poll();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
